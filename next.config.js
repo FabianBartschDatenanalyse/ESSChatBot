3c21c7686d328965da931fcf2ee51c021856a5a5
Fabian Bartsch <AhoiDerPingu@gmail.com>
2025-07-26 20:13:20 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ next.config.js
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+   webpack: (config, { isServer }) => {
+    // This is to fix a build error with tensorflow.js.
+    // It's a workaround for a known issue.
+    config.resolve.alias['supports-color'] = false;
+    return config;
+  },
+};
+
+module.exports = nextConfig;
